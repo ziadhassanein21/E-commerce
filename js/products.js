a1d901fb19f5b1852cd72d82b34329eb32a56d52
Ziad Hassanein <ziadhassanein21@gmail.com>
2026-04-20 07:20:51 +0200

    feat: add product data module and update product page navigation links

--- js/products.js
+++ js/products.js
@@ -9,9 +9,9 @@ export const products = [
         description: "A full-bodied, mysterious and sensual interpretation of Delina. Opening with pear, lychee and grapefruit, the heart dives into a bouquet of Turkish rose, incense and vetiver.",
         volumes: [75],
         images: [
-            "../assets/images/DELINA_EXCLUSIF-PERFUME-75-PACK1-1X1_CENTERED_3_f30161f3-b0cf-4260-8b5b-8213b0819e5f.webp",
-            "../assets/images/DELINA-EXCLUSIF-PERFUME-75-PACK2-1X1_669a30f7-fcae-4a6a-aa24-c8ee427a2739.webp",
-            "../assets/images/DELINA-EXCLUSIF-PERFUME-75-PACK3-1x1_e8735b8d-0c47-4087-953d-7d08bec95598.webp"
+            "./assets/images/DELINA_EXCLUSIF-PERFUME-75-PACK1-1X1_CENTERED_3_f30161f3-b0cf-4260-8b5b-8213b0819e5f.webp",
+            "./assets/images/DELINA-EXCLUSIF-PERFUME-75-PACK2-1X1_669a30f7-fcae-4a6a-aa24-c8ee427a2739.webp",
+            "./assets/images/DELINA-EXCLUSIF-PERFUME-75-PACK3-1x1_e8735b8d-0c47-4087-953d-7d08bec95598.webp"
         ],
         details: {
             topNotes: "Pear, Lychee, Grapefruit",
@@ -32,10 +32,10 @@ export const products = [
         description: "A woodier and spicier take on the classic Layton. Generous, spirited and explosive, this parfum asserts its strength and impetuousness.",
         volumes: [75, 125],
         images: [
-            "../assets/images/LAYTON_EXCLUSIF-PERFUME-125-PACK1-1X1_CENTERED_618a34f4-9fa5-4b41-b764-ed01330eace0.webp",
-            "../assets/images/LAYTON-EXCLUSIF-PERFUME-75-PACK2_1x1_4448f090-8834-4898-9aca-6ae231826394.webp",
-            "../assets/images/LAYTON-EXCLUSIF-PERFUME-125-PACK3-1x1_1_8d88aa83-c522-446d-ae25-f9a688ca16aa.webp",
-            "../assets/images/LAYTON-EXCLUSIF-PERFUME-125-PACK4-1x1_1_f3743595-7101-45c8-8e9d-e1c9b3852ce7.webp"
+            "./assets/images/LAYTON_EXCLUSIF-PERFUME-125-PACK1-1X1_CENTERED_618a34f4-9fa5-4b41-b764-ed01330eace0.webp",
+            "./assets/images/LAYTON-EXCLUSIF-PERFUME-75-PACK2_1x1_4448f090-8834-4898-9aca-6ae231826394.webp",
+            "./assets/images/LAYTON-EXCLUSIF-PERFUME-125-PACK3-1x1_1_8d88aa83-c522-446d-ae25-f9a688ca16aa.webp",
+            "./assets/images/LAYTON-EXCLUSIF-PERFUME-125-PACK4-1x1_1_f3743595-7101-45c8-8e9d-e1c9b3852ce7.webp"
         ],
         details: {
             topNotes: "Bergamot, Grapefruit, Apple",
@@ -56,9 +56,9 @@ export const products = [
         description: "A modern ode to high-quality Bourbon vanilla, blended with citrus, spices, and noble woods for a warm, addictive trail.",
         volumes: [75, 125],
         images: [
-            "../assets/images/ALTHAIR-PERFUME-75-PACK1-1X1_CENTERED_8e536321-9220-4ece-91fd-de6b13f6896d.webp",
-            "../assets/images/ALTHAIR-PERFUME-125-PCK2_1x1_2b310274-54c5-4035-a913-6c70c71e50db.webp",
-            "../assets/images/ALTHAIR-PERFUME-75-PACK3-1X1png_1_40761c0e-326c-49fd-879c-6fd063ccf8c2.webp"
+            "./assets/images/ALTHAIR-PERFUME-75-PACK1-1X1_CENTERED_8e536321-9220-4ece-91fd-de6b13f6896d.webp",
+            "./assets/images/ALTHAIR-PERFUME-125-PCK2_1x1_2b310274-54c5-4035-a913-6c70c71e50db.webp",
+            "./assets/images/ALTHAIR-PERFUME-75-PACK3-1X1png_1_40761c0e-326c-49fd-879c-6fd063ccf8c2.webp"
         ],
         details: {
             topNotes: "Orange Blossom, Bergamot, Cinnamon",
@@ -79,9 +79,9 @@ export const products = [
         description: "An original scent of both light and shadow, featuring aromatic woods, saffron and praline twist for a contemporary spin on oud.",
         volumes: [75, 125],
         images: [
-            "../assets/images/HALTHANE-PERFUME-125-PACK1-1X1_CENTERED_1_de971446-66fe-402e-8f51-e87154a05382.webp",
-            "../assets/images/HALTANE-PERFUME-75-PACK2_1X1_1_28610b32-6999-415b-b9b0-4809a059a6b6.webp",
-            "../assets/images/HALTANE-PERFUME-125-PACK3-1x1_1_f6438466-608e-4149-a396-83a31fbfb519.webp"
+            "./assets/images/HALTHANE-PERFUME-125-PACK1-1X1_CENTERED_1_de971446-66fe-402e-8f51-e87154a05382.webp",
+            "./assets/images/HALTANE-PERFUME-75-PACK2_1X1_1_28610b32-6999-415b-b9b0-4809a059a6b6.webp",
+            "./assets/images/HALTANE-PERFUME-125-PACK3-1x1_1_f6438466-608e-4149-a396-83a31fbfb519.webp"
         ],
         details: {
             topNotes: "Bergamot, Lavender, Clary Sage",
@@ -102,9 +102,9 @@ export const products = [
         description: "A delicate cocktail of fruit and flowers with a romantic heart of rose, lily and ylang-ylang. Fresh, airy, and sparkling.",
         volumes: [75],
         images: [
-            "../assets/images/MELIORA-PERFUME-75-PACK1-1X1-_CENTERED_574884e5-cdb3-4cea-9cd5-19a8e5d1b17e.webp",
-            "../assets/images/MELIORA-PERFUME-75-PACK2_1X1_9e8f5ad8-6ea4-47b8-b4d3-b2215f115fff.webp",
-            "../assets/images/MELIORA-PERFUME-75-PACK3-1X1_2_d692efb4-0581-4d18-917f-e0eb986ad3df.webp"
+            "./assets/images/MELIORA-PERFUME-75-PACK1-1X1-_CENTERED_574884e5-cdb3-4cea-9cd5-19a8e5d1b17e.webp",
+            "./assets/images/MELIORA-PERFUME-75-PACK2_1X1_9e8f5ad8-6ea4-47b8-b4d3-b2215f115fff.webp",
+            "./assets/images/MELIORA-PERFUME-75-PACK3-1X1_2_d692efb4-0581-4d18-917f-e0eb986ad3df.webp"
         ],
         details: {
             topNotes: "Red Berries, Raspberry, Lemon Essence",
@@ -125,9 +125,9 @@ export const products = [
         description: "A heady fern scent with almond and vanilla, adding a touch of sweetness to the traditional masculinity of sandalwood and amber.",
         volumes: [75, 125],
         images: [
-            "../assets/images/PEGASUS-PERFUME-75-PACK1-1X1_CENTERED_54e9798a-d4d6-4527-988c-d0927f891047.webp",
-            "../assets/images/PEGASUS-PERFUME-125-PACK2_1X1_db4e62e6-d00c-4820-b800-4826ef7b5d7a.webp",
-            "../assets/images/PEGASUS-PERFUME-125-PACK3-1x1_2_0b5f418d-3490-433e-afcc-007dbd88c8d8.webp"
+            "./assets/images/PEGASUS-PERFUME-75-PACK1-1X1_CENTERED_54e9798a-d4d6-4527-988c-d0927f891047.webp",
+            "./assets/images/PEGASUS-PERFUME-125-PACK2_1X1_db4e62e6-d00c-4820-b800-4826ef7b5d7a.webp",
+            "./assets/images/PEGASUS-PERFUME-125-PACK3-1x1_2_0b5f418d-3490-433e-afcc-007dbd88c8d8.webp"
         ],
         details: {
             topNotes: "Cypress, Bergamot, Heliotrope",
@@ -148,9 +148,9 @@ export const products = [
         description: "Warm and velvety, Safanad reveals orange blossom, iris and ylang-ylang trailed by amber, sandalwood and vanilla.",
         volumes: [75],
         images: [
-            "../assets/images/SAFANAD-PERFUME-75-PACK1-1X1_CENTERED_1_4d79f6a4-8817-4950-b373-b732fd91561d.webp",
-            "../assets/images/SAFANAD-PERFUME-75-PACK2_1X1_0f435b12-3c89-441f-83bf-919a02efef8b.webp",
-            "../assets/images/SAFANAD-PERFUME-75-PACK3-1X1_2_489125ca-6e9f-4625-9a95-984595868605.webp"
+            "./assets/images/SAFANAD-PERFUME-75-PACK1-1X1_CENTERED_1_4d79f6a4-8817-4950-b373-b732fd91561d.webp",
+            "./assets/images/SAFANAD-PERFUME-75-PACK2_1X1_0f435b12-3c89-441f-83bf-919a02efef8b.webp",
+            "./assets/images/SAFANAD-PERFUME-75-PACK3-1X1_2_489125ca-6e9f-4625-9a95-984595868605.webp"
         ],
         details: {
             topNotes: "Orange, Pear, Petitgrain",
@@ -171,9 +171,9 @@ export const products = [
         description: "A grand gourmand. Honey and cinnamon weave together with osmanthus and amber for an unforgettable, sweet-spicy sillage.",
         volumes: [125],
         images: [
-            "../assets/images/OAJAN-PERFUME-125-PACK1-1X1_CENTERED_3ee2f0e2-56f7-40ed-9f99-b13282837433.webp",
-            "../assets/images/OAJAN-PERFUME-125-PACK2_1x1_41c19340-2562-418d-90ba-e8bec6e518a8.webp",
-            "../assets/images/OAJAN-PERFUME-125-PACK3-1x1_1_ef1c08be-a7e6-477d-b43d-9f2c7810a309.webp"
+            "./assets/images/OAJAN-PERFUME-125-PACK1-1X1_CENTERED_3ee2f0e2-56f7-40ed-9f99-b13282837433.webp",
+            "./assets/images/OAJAN-PERFUME-125-PACK2_1x1_41c19340-2562-418d-90ba-e8bec6e518a8.webp",
+            "./assets/images/OAJAN-PERFUME-125-PACK3-1x1_1_ef1c08be-a7e6-477d-b43d-9f2c7810a309.webp"
         ],
         details: {
             topNotes: "Honey, Cinnamon, Osmanthus",
@@ -194,9 +194,9 @@ export const products = [
         description: "Intense and subtle at once. Kalan surprises with a sparkling opening of blood orange and spices, grounded by lavender and wood.",
         volumes: [75, 125],
         images: [
-            "../assets/images/KALAN-PERFUME-125-PACK1-1X1_CENTERED_1_da758f8a-41ba-4c80-9fcb-c255e09b2d9d.webp",
-            "../assets/images/KALAN-PERFUME-125-PACK2_1x1_1_13d9133d-ca6e-4230-bc40-a6324343c686.webp",
-            "../assets/images/KALAN-PERFUME-125-PACK3-1x1_2_2d9636c9-5343-4f22-bda8-dd0eee45ef15.webp"
+            "./assets/images/KALAN-PERFUME-125-PACK1-1X1_CENTERED_1_da758f8a-41ba-4c80-9fcb-c255e09b2d9d.webp",
+            "./assets/images/KALAN-PERFUME-125-PACK2_1x1_1_13d9133d-ca6e-4230-bc40-a6324343c686.webp",
+            "./assets/images/KALAN-PERFUME-125-PACK3-1x1_2_2d9636c9-5343-4f22-bda8-dd0eee45ef15.webp"
         ],
         details: {
             topNotes: "Blood Orange, Black Pepper, Spices",
@@ -207,4 +207,5 @@ export const products = [
             concentration: "Eau de Parfum"
         }
     }
-];
+],
+
